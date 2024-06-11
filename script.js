88c945fd79b138815bb36a11b1860ad44686770c
Charlie Turner <c.w.turner@hotmail.com>
2024-06-11 20:32:55 +0100

    Add click event listener to R and L nav

--- script.js
+++ script.js
@@ -10,3 +10,14 @@
 // function revealHidden() {
 //   document.querySelector(".hidden").style.display = "block";
 // }
+
+const leftNav = document.querySelector(".left");
+const rightNav = document.querySelector(".right");
+
+rightNav.addEventListener("click", () => {
+  console.log("Hello World!");
+});
+
+leftNav.addEventListener("click", () => {
+  console.log("Hello World!");
+});
