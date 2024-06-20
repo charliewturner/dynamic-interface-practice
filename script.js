49468b30be5edd15431eddb2a7b30219c0a1f2ee
Charlie Turner <c.w.turner@hotmail.com>
2024-06-20 14:55:39 +0100

    Add slideshow timer

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 const leftNav = document.querySelector(".left");
 const rightNav = document.querySelector(".right");
 const gallery = document.querySelector(".gallery");
-const navDots = document.querySelector(".nav-dot");
 
-// navDots.addEventListener("click", () => {
-//   if (navDots.id == "off") {
-//     navDots.id = "on";
-//   }
-// });
-
-// function dotNavigate() {
-//     var dots = document.getElementsByClassName("nav-dot");
-
-// }
+//Left and right arrow functions
 
 rightNav.addEventListener("click", () => {
   let galleryID = gallery.id;
@@ -68,15 +58,9 @@ leftNav.addEventListener("click", () => {
   }
 });
 
-const navDotContainer = document.getElementById("container");
+//Nav dot functionality
 
-// const dot1 = document.getElementById("nav-dot-1");
-// const dot2 = document.getElementById("nav-dot-2");
-// const dot3 = document.getElementById("nav-dot-3");
-// const dot4 = document.getElementById("nav-dot-4");
-// const dot5 = document.getElementById("nav-dot-5");
-// const dot6 = document.getElementById("nav-dot-6");
-// const dot7 = document.getElementById("nav-dot-7");
+const navDotContainer = document.getElementById("container");
 
 document.getElementById("nav-dot-1").addEventListener("click", () => {
   navDotContainer.className = "nav-dot-container first_selected";
@@ -107,32 +91,44 @@ document.getElementById("nav-dot-7").addEventListener("click", () => {
   navDotContainer.className = "nav-dot-container seventh_selected";
   gallery.id = "position-7";
 });
-// dot2.addEventListener("click", () => {
-//   gallery.id = "position-2";
-//   navDotContainer.id = "active";
-// });
 
-// dot3.addEventListener("click", () => {
-//   gallery.id = "position-3";
-//   navDotContainer.id = "active";
-// });
+//Slideshow timer
 
-// dot4.addEventListener("click", () => {
-//   gallery.id = "position-4";
-//   navDotContainer.id = "active";
-// });
+function changeImage() {
+  let galleryID = gallery.id;
 
-// dot5.addEventListener("click", () => {
-//   gallery.id = "position-5";
-//   navDotContainer.id = "active";
-// });
+  switch (galleryID) {
+    case "position-1":
+      gallery.id = "position-2";
+      navDotContainer.className = "nav-dot-container second_selected";
+      break;
+    case "position-2":
+      gallery.id = "position-3";
+      navDotContainer.className = "nav-dot-container third_selected";
+      break;
+    case "position-3":
+      gallery.id = "position-4";
+      navDotContainer.className = "nav-dot-container fourth_selected";
+      break;
+    case "position-4":
+      gallery.id = "position-5";
+      navDotContainer.className = "nav-dot-container fifth_selected";
+      break;
+    case "position-5":
+      gallery.id = "position-6";
+      navDotContainer.className = "nav-dot-container sixth_selected";
+      break;
+    case "position-6":
+      gallery.id = "position-7";
+      navDotContainer.className = "nav-dot-container seventh_selected";
+      break;
+    case "position-7":
+      gallery.id = "position-1";
+      navDotContainer.className = "nav-dot-container first_selected";
+      break;
+    default:
+      console.log("Cannot navigate further left");
+  }
+}
 
-// dot6.addEventListener("click", () => {
-//   gallery.id = "position-6";
-//   navDotContainer.id = "active";
-// });
-
-// dot7.addEventListener("click", () => {
-//   gallery.id = "position-7";
-//   navDotContainer.id = "active";
-// });
+setInterval("changeImage()", 5000);
